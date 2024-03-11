e886f21ad514ec7d78ec8be45a4afe30f8e9751a
lfdelgadom <lfdelgadom@unal.edu.co>
2024-03-10 20:26:54 -0500

    code commented

--- script/case_example_CO2.R
+++ script/case_example_CO2.R
@@ -1,5 +1,25 @@
-# Load CO2 DATA
-library(readr)
-CO2 <- read_delim("data/CO2.csv", delim = "\t", 
-                  escape_double = FALSE, trim_ws = TRUE)
-View(CO2)
+# ---- CARGA DE DATOS DE CO2 ----
+# Carga las bibliotecas necesarias para la manipulación y visualización de datos.
+library(readr) # Para leer archivos de datos.
+library(tidyverse) # Para manipulación de datos y gráficos.
+
+# Lee los datos del archivo CO2.csv, asumiendo que los datos están delimitados por tabuladores.
+Co2 <- read_delim("data/CO2.csv", 
+                  delim = "\t", 
+                  escape_double = FALSE, 
+                  trim_ws = TRUE)
+
+# Utiliza View para inspeccionar los datos en un formato tabular dentro del RStudio.
+View(Co2)
+
+# ---- VISUALIZACIÓN DE DATOS DE CO2 ----
+# Crea un gráfico boxplot para visualizar la distribución de la absorción (uptake) de CO2,
+# agrupado por tratamiento (Treatment), y luego divide los datos en facetas basadas en el tipo (Type).
+Co2 %>%
+  ggplot(aes(x = Treatment, y = uptake)) + # Define las estéticas: x como tratamiento y y como absorción.
+  geom_boxplot() + # Añade un boxplot para mostrar la distribución de los datos.
+  geom_jitter(width = 0.1) + # Añade puntos de datos individuales como "jitter" para evitar superposición.
+  facet_wrap(~Type) # Divide el gráfico en paneles separados para cada tipo.
+
+
+
--- script/graphics_ggplot2.R
+++ script/graphics_ggplot2.R
@@ -1,177 +1,151 @@
-# ---- INSTALL GGPLOT2 ----
-  #install.packages("ggplot2")
-  library(ggplot2)
+# ---- INSTALACIÓN DE GGPLOT2 ----
+# La siguiente línea, si se descomenta, instalará ggplot2, una biblioteca para la visualización de datos.
+#install.packages("ggplot2")
+library(ggplot2) # Carga la biblioteca ggplot2.
 
-# IRIS DATASET ----
+# ---- CONJUNTO DE DATOS IRIS ----
 
-data(iris)
-summary(iris) # resumen de la base de datos
-head(iris, 10) # muestra las primeras 6 entradas de la base de datos
-tail(iris)
-dim(iris)
+data(iris) # Carga el conjunto de datos iris.
+summary(iris) # Muestra un resumen estadístico del conjunto de datos iris.
+head(iris, 10) # Muestra las primeras 10 filas del conjunto de datos iris.
+tail(iris) # Muestra las últimas 6 filas del conjunto de datos iris.
+dim(iris) # Muestra las dimensiones del conjunto de datos iris.
 
-# PLOT IRIS ----
-## plot pequeno ----
-plot(iris)
-plot(iris$Petal.Length, iris$Sepal.Width) # petal length vs sepal width
+# ---- GRÁFICOS DEL CONJUNTO DE DATOS IRIS ----
+# Gráficos básicos usando las funciones de plot de base en R.
+plot(iris) # Crea un gráfico de pares para todas las variables en el conjunto de datos iris.
+plot(iris$Petal.Length, iris$Sepal.Width) # Gráfico de dispersión entre longitud del pétalo y ancho del sépalo.
 plot(iris$Petal.Width, iris$Sepal.Length, 
-     ylab = "Longitud del sepalo (cm)", 
-     xlab = "Ancho del petalo (cm)", 
-     col = "purple") 
+     ylab = "Longitud del sépalo (cm)", 
+     xlab = "Ancho del pétalo (cm)", 
+     col = "purple") # Gráfico de dispersión personalizado con colores y etiquetas.
 
-# GGPLOT IRIS ----
+# ---- VISUALIZACIÓN CON GGPLOT2 ----
 
-# ggplot(data_set, mapping(aes(x = varx, y = vary))) +
-#geometrias
-
-#point
+# Gráfico de puntos básico.
 ggplot(iris, aes(x = Petal.Length, y = Sepal.Width)) +
   geom_point()
 
-#jitter <- ruido aleatorio
+# Gráfico de puntos con jitter (ruido aleatorio) para evitar superposiciones.
 ggplot(iris, aes(x = Petal.Length, y = Sepal.Width)) +
-  geom_jitter(width = 0.3,
-              height = 0.5)
+  geom_jitter(width = 0.3, height = 0.5)
 
-#facets
+# Gráfico de puntos con facetas para cada especie del conjunto de datos iris.
 ggplot(iris, aes(x = Petal.Length, y = Sepal.Width)) +
   geom_point() +
   facet_wrap(~Species)
 
-# statistics
-#facets
+# Añadiendo líneas de tendencia lineal por especie sin bandas de confianza en color rojo.
 ggplot(iris, aes(x = Petal.Length, y = Sepal.Width)) +
   geom_point() +
   facet_wrap(~Species) +
-  geom_smooth(method = "lm", se = F, col = "red")
+  geom_smooth(method = "lm", se = FALSE, col = "red")
 
-# Add trend line non linear
+# Ejemplo de cómo personalizar escalas y temas en ggplot.
 ggplot(iris, aes(x = Petal.Length, y = Sepal.Width)) +
   geom_point() +
   facet_wrap(~Species) +
-  geom_smooth(se = F, col = "red")
+  geom_smooth(method = "lm", se = FALSE, col = "red") +
+  scale_y_continuous("Ancho del Sépalo", breaks = seq(0, 5, 1)) +
+  theme_bw() # Aplica un tema en blanco y negro.
 
-# coordinates
-ggplot(iris, aes(x = Petal.Length, y = Sepal.Width)) +
-  geom_point() +
-  facet_wrap(~Species) +
-  geom_smooth(method = "lm", se = F, col = "red") +
-  scale_y_continuous("Sepal Width", breaks = seq(0, 5, 2))
+# ---- PERSONALIZACIÓN DEL COLOR ----
 
-# theme
-ggplot(iris, aes(x = Petal.Length, y = Sepal.Width)) +
-  geom_point() +
-  facet_wrap(~Species) +
-  geom_smooth(method = "lm", se = F, col = "red") +
-  scale_y_continuous("Sepal Width", breaks = seq(0,5, 1)) +
-  theme_bw()
-
-
-?geom_point
-
-# color
-ggplot(iris, aes(x = Petal.Length, y = Sepal.Width, col = Species)) +
+# Gráfico de puntos coloreados por especie.
+ggplot(iris, aes(x = Petal.Length, y = Sepal.Width, color = Species)) +
   geom_point()
 
-ggplot(iris, aes(x = Petal.Length, y = Sepal.Width, col = Species, 
-                 size = Petal.Width)) +
+# Gráfico de puntos con tamaño de puntos basado en el ancho del pétalo.
+ggplot(iris, aes(x = Petal.Length, y = Sepal.Width, color = Species, size = Petal.Width)) +
   geom_point()
 
-plot_1 <- ggplot(iris, aes(x = Petal.Length, y = Sepal.Width, col = Species, 
-                 size = Petal.Width, shape = Species)) +
+# Gráfico de puntos con jitter, coloreado por especie, tamaño basado en el ancho del pétalo y forma basada en la especie.
+plot_1 <- ggplot(iris, aes(x = Petal.Length, y = Sepal.Width, color = Species, size = Petal.Width, shape = Species)) +
   geom_jitter()
 
-# save the last plot
+# ---- GUARDAR EL ÚLTIMO GRÁFICO ----
 ggsave(
-  paste0("images/", "scatter_plot_iris.png"),
-  width = 8,
-  height = 6,
-  units = "in",
-  dpi = 300
+  filename = "images/scatter_plot_iris.png",
+  plot = plot_1, # Guarda el gráfico almacenado en plot_1.
+  width = 8, height = 6, units = "in", dpi = 300
 )
 
 
+# ---- CREACIÓN Y VISUALIZACIÓN DE UN GRÁFICO COMPLEJO CON GGPLOT2 ----
 grafico_completo <- ggplot(iris, aes(x = Petal.Length, y = Sepal.Width, 
                                      col = Species, size = Petal.Width, 
                                      shape = Species, alpha = Sepal.Length)) +
-  geom_point()
-grafico_completo
+  geom_point() # Gráfico de puntos con varias estéticas personalizadas.
+grafico_completo # Muestra el gráfico completo.
 
-# save complete graph
+# ---- GUARDAR EL GRÁFICO COMPLETO ----
 ggsave(
-  paste0("images/", "scatter_complete_iris.png"),
-  plot = grafico_completo,
-  width = 8,
-  height = 6,
-  units = "in",
-  dpi = 300
+  filename = "images/scatter_complete_iris.png",
+  plot = grafico_completo, # Guarda el gráfico completo en un archivo.
+  width = 8, height = 6, units = "in", dpi = 300
 )
 
+# ---- CONTAR LA FRECUENCIA DE ESPECIES EN EL DATASET IRIS ----
+#count(iris, Species) # Cuenta la frecuencia de cada especie en el conjunto de datos iris.
 
-#count(iris, Species)
+# ---- HACER INTERACTIVO EL GRÁFICO COMPLETO CON PLOTLY ----
+#install.packages("plotly") # Instala el paquete Plotly si es necesario.
+library(plotly) # Carga la biblioteca Plotly.
+plotly::ggplotly(plot_1) # Convierte el gráfico de ggplot2 en una versión interactiva con Plotly.
 
-#install.packages("plotly")
-library(plotly)
+# ---- BARRAS Y BOXPLOTS CON GGPLOT2 Y DPLYR ----
+library(dplyr) # Carga la biblioteca dplyr para manipulación de datos.
 
-#volver interactico el grafico completo
-plotly::ggplotly(plot_1)
+# Resumen de longitud de sépalos y pétalos por especie.
+resumen <- iris %>%
+  group_by(Species) %>%
+  summarise(mean_sepal_length = mean(Sepal.Length), mean_petal_length = mean(Petal.Length))
 
-
-# BAR AND BOXPLOT ----
-library(dplyr)
-
-# summary petals and sepals
-resumen <- iris %>% group_by(Species) %>% 
-  summarise(mean_sepal_length = mean(Sepal.Length), 
-            mean_petal_length = mean(Petal.Length))
-
+# Gráfico de columnas para longitud media de sépalos por especie.
 ggplot(resumen, aes(x = Species, y = mean_sepal_length)) +
   geom_col()
 
+# Diferentes formas de crear gráficos de barras para la longitud media de sépalos por especie.
 ggplot(iris, aes(x = Species, y = Sepal.Length)) +
   geom_bar(stat = "summary", fun.y = "mean")
-
 ggplot(iris, aes(Species, Sepal.Length, fill = Species)) +
   geom_bar(stat = "summary", fun.y = "mean")
-
 ggplot(iris, aes(Species, Sepal.Length)) +
   geom_bar(stat = "summary", fun.y = "mean", fill = "blue")
-
 ggplot(iris, aes(Species, Sepal.Length)) +
   geom_bar(stat = "summary", fun.y = "mean", fill = "#00166e", col = "black") + 
   geom_boxplot()
 
+# Creación y guardado de un boxplot con jitter para longitud de sépalos por especie.
 bxp <- ggplot(iris, aes(Species, Sepal.Length, fill = Species)) +
   geom_boxplot() +
   geom_jitter(width = 0.1)
+ggsave(paste0("images/", "bxp.png"), plot = bxp, dpi = 300, height = 6, width = 8)
 
-ggsave(paste0("images/", "bxp.png"),  plot = bxp, dpi = 300, height = 6, width = 8)
-
-#Las diferentes formas de los puntos de R
-
-d=data.frame(p=c(0:25))
+# ---- DEMOSTRACIÓN DE LAS DIFERENTES FORMAS DE PUNTOS EN R ----
+d = data.frame(p = c(0:25))
 ggplot() +
-  scale_y_continuous(name="") +
-  scale_x_continuous(name="") +
+  scale_y_continuous(name = "") +
+  scale_x_continuous(name = "") +
   scale_shape_identity() +
-  geom_point(data=d, mapping=aes(x=p%%16, y=p%/%16, shape=p), size=5, fill="red") +
-  geom_text(data=d, mapping=aes(x=p%%16, y=p%/%16+0.25, label=p), size=3)
+  geom_point(data = d, mapping = aes(x = p%%16, y = p%/%16, shape = p), size = 5, fill = "red") +
+  geom_text(data = d, mapping = aes(x = p%%16, y = p%/%16 + 0.25, label = p), size = 3)
 
+# Personalización de temas y combinación de geométricos.
 
-ggplot(iris, aes(Petal.Width, Sepal.Length)) +
-   geom_jitter(size = 2, shape = 11)
-
+# ---- CREACIÓN DE UN GRÁFICO PERSONALIZADO CON GGPLOT2 ----
+# Este bloque crea un gráfico de barras que muestra la longitud media de los sépalos por especie en el conjunto de datos iris,
+# y añade un gráfico de dispersión (jitter) para visualizar la distribución de los datos individuales.
 myPlot <- ggplot(iris, aes(Species, Sepal.Length)) +
   geom_bar(stat = "summary", fun.y = "mean", fill = "#8cb87b", col = "black") +
   geom_jitter(size = 4, aes(shape = Species, color = Species))
 
+# Personalización del tema del gráfico para mejorar la estética.
 myPlot + theme(panel.grid = element_blank(),
                panel.background = element_rect(fill = "white"),
                panel.border = element_rect(colour = "black", fill = NA, size = 0.2))
 
-?theme
-
-myPlot 
+# Aplicación de diferentes temas a `myPlot` para explorar estilos visuales.
 myPlot + theme_bw()
 myPlot + theme_classic()
 myPlot + theme_dark()
@@ -181,41 +155,40 @@ myPlot + theme_linedraw() + theme(panel.background = element_rect(fill = "blue")
 myPlot + theme_minimal()
 myPlot + theme_void()
 
-ggplot(iris, aes(x = Species, y = Sepal.Length, fill = Species)) +
-  geom_boxplot(col = "black", notch = ) +
-  geom_jitter(with = 0.1, alpha = 0.5) 
-
+# Creación de un boxplot para la longitud de los sépalos por especie en el conjunto de datos iris, incluyendo una visualización de los datos individuales con jitter.
+# Se corrige la línea con el parámetro 'notch' incompleto en la versión original.
 ggplot(iris, aes(x = Species, y = Sepal.Length, fill = Species)) +
   geom_boxplot(col = "black", notch = TRUE) +
-  geom_jitter(with = 0.1, alpha = 0.5)  +
+  geom_jitter(width = 0.1, alpha = 0.5) +
   theme_bw() +
-  labs(title = "Iris's boxplot", caption = "Universidad Nacional de Colombia",
-       y = "Longitud de sepalo (cm)") +
+  labs(title = "Boxplot del conjunto de datos Iris", caption = "Universidad Nacional de Colombia",
+       y = "Longitud de sépalo (cm)") +
   theme(plot.title = element_text(hjust = 0.5))
 
-# FINISHING TOUCHES ----
+# ---- TOQUES FINALES ----
+# Se aplican toques finales al gráfico `myPlot`, incluyendo un cambio de tema y ajustes de etiquetas y título.
 myPlot + 
   theme_classic() +
-  labs(x = "", y = "Sepal length (mm)") +
-  ggtitle("Sepal length by iris species") +
+  labs(x = "", y = "Longitud del sépalo (mm)") +
+  ggtitle("Longitud del sépalo por especie de Iris") +
   theme(plot.title = element_text(hjust = 0.5))
 
-# SAVING OUR PLOT ----
-#setwd("~/Documents")
+# ---- GUARDAR EL GRÁFICO ----
 ggsave("Graph 1.png", width = 8, height = 5)
 
-# FACTORIAL ----
-
+# ---- VISUALIZACIÓN DEL CONJUNTO DE DATOS TOOTHGROWTH ----
+# Carga y revisión preliminar del conjunto de datos ToothGrowth.
 data(ToothGrowth)
 head(ToothGrowth)
 summary(ToothGrowth)
-?ToothGrowth
 
+# Creación de gráficos para explorar el efecto del suplemento y la dosis en el crecimiento del diente.
+# Primer gráfico: Barras representando la mediana de longitud de diente por tipo de suplemento, con diferenciación por dosis.
 ggplot(ToothGrowth, aes(supp, len, fill = as.factor(dose))) +
   geom_bar(stat = "summary", fun.y = "median", col = "black", position = "dodge") +
   geom_point(position = position_dodge(0.9))
 
+# Segundo gráfico: Líneas representando la media de longitud de diente por dosis, diferenciadas por tipo de suplemento.
 ggplot(ToothGrowth, aes(as.factor(dose), len, group = supp, col = supp)) +
   geom_line(stat = "summary", fun.y = "mean") +
   geom_smooth(method = "lm")
-
